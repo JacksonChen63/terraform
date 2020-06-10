b670476c5109390c9c210406c52d7e88acf682e8
root <root@alertmanager123.lab.cht.bxvip>
2020-06-10 22:13:53 +0800

    edit terraform build instance

--- aws/main.tf
+++ aws/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "jackson" {
   ami           = "${var.ami}"
   instance_type = "${var.instance_type}"
-  vpc_security_group_ids = "[${var.vpc_security_group_ids}]" 
+  vpc_security_group_ids = ["${var.vpc_security_group_ids}"]
 }
