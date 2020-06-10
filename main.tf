f6a96b0bd4d68147b13b4fc02560e3469ee856e9
YENHAO <37585940+JacksonCHEN63@users.noreply.github.com>
2020-06-10 16:17:31 +0800

    Delete main.tf

--- main.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_instance" "${var.instance_name}" {
-  ami           = "${var.ami}"
-  instance_type = "${var.instance_type}"
-  vpc_security_group_ids = "${var.vpc_security_group_ids}" 
